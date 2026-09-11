19f5ed4f623d7b433edcd631492244100a3b887d
Apprentice Dev <jangtzunghan@gmail.com>
2026-09-11 15:50:36 +0800

    Progress: unfinished PE transitions. Proposing to use C as main driver, and asm as util functions. Transition from 16 to 32 bit mode C code or vice versa, using asm.

--- stage2.asm
+++ stage2.asm
@@ -1,8 +1,10 @@
 bits 16
 
 global _start
-extern lol
+;extern lol
 global puts
+
+extern main
 ;transition to 32 bit mode then drop down to 16 bit unreal mode when doing graphics
 
 section .text
@@ -17,18 +19,29 @@ mov fs, ax
 xor sp, sp
 mov sp, 0x7c00 
 
+;transfer control to c???
+jmp dword main
+
 ;mov di, debug
 sub esp, 4
 mov DWORD [esp], debug
 call dword puts
 add esp,4
-call dword lol
+
+jmp dword main
+
+;call dword lol
+
+
+
+
 
 halt:
 hlt
 jmp halt
 
 
+;itanium i386 abi (32 bit for gcc)
 puts: ;hacky function for C since gcc uses 32 bit override in 16 bit for their internals
 mov edi, [esp+4]
 jmp .loop
@@ -43,7 +56,59 @@ jnz .cloop
 retd
 
 
+;should only use jmp
+;should be executing in 16 bit mode prior
+;itanium i386 abi (32 bit for gcc interop)
+;must supply a return address (avoid ambigutations between calling inside same asm snippet vs calling in c with 0 autonomy)
+;extern "C" void enter_protected_mode(void (*ret)())
+;input: 32 bit mode
+;output:16 bit mode
+;
+;
+enter_protected_mode:
+
+
+;should only use jmp
+;should be executing in 32 bit mode prior
+;itanium i386 also
+;extern "C" void exit_protected_mode(void (*ret)())
+;input:32 bit mode
+;output:16 bit mode
+;
+;
+exit_protected_mode: ;not unreal mode only real. will reset the limit back smm breaks unreal anyway 
+
+
+
 
 section .data
 ;sixseven db 67 dup (67)
 debug db "DEBUG: Stage 2 bootloader successfully loaded.",0xd,0xA,0x0
+PEGDT_INFO:
+dw 8*3-1 ;3 entries and minus the first bit for maximum offset
+dd PEGDT
+PEGDT:
+dq 0 
+.code_desc
+dw 0xffff
+dw 0
+db 0
+db 0b10011011
+db 0b11001111 ;full pe unlock CS, and in unreal this would be 00001111 can throw away granularity too in unreal
+db 0
+.data_desc
+dw 0xffff
+dw 0
+db 0
+db 0b10010011
+db 11001111 ;full unlock 4gb for full pe
+db 0
+;downgrade :wilted_rose:
+REALGDT_INFO:
+dw 8*3-1
+dd PREALGDT
+REALGDT:
+dq 0
+.code_desc
+.data_desc
+
--- useless_c.c
+++ useless_c.c
@@ -1,8 +1,8 @@
 
-extern void puts(char *const str);
+extern void puts(char *const str); //hate header files
 
 char *lolxd = "haha idk what im doing\r\n\0";
 
-/*extern*/ void lol(){
+/*extern*/ void main(){
   puts(lolxd);
 }
